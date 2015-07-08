4a4056c5a7148db2d613338754d15fbb5bec1c2c
DM <kingcocomango@gmail.com>
2015-07-08 10:46:03 +0200

    Time for logistic regression. outline

---
+++ log.m
@@ -0,0 +1,11 @@
+% Learn from experience E how to perform better at task T as measured by C
+
+% Calculate results given weights+variables
+
+% Cost function
+
+% Gradient Descent 1-Batch Step
+
+% Gradient Descent
+
+% Example Data
