a8c833762caa670bd88b5a5a6cb08c2319833f14
DM <kingcocomango@gmail.com>
2015-07-08 05:28:28 +0200

    simple lin reg gd outline

---
+++ linear.m
@@ -0,0 +1,12 @@
+% Learn from experience E how to perform better at task T as measured by C
+% Simple linear first. Bias + 1 variable.
+
+% Calculate a results given weights+data.
+
+% Draw line as plot, given weights data, use above to get results needed.
+
+% Cost function, cost given results & targets.
+% Mean Squared Error cost function.
+
+% Gradient descent for the above.
+
