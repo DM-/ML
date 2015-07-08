dd07dddbde51373df95b09adb009b987239545bf
DM <kingcocomango@gmail.com>
2015-07-08 09:26:26 +0200

    Yep, it works. Now to test it with external datasets and make sure!

--- flinear.m
+++ flinear.m
@@ -1,23 +1,18 @@
 % Learn from experience E how to perform better at task T as measured by C
-% any no of variables. 2 weights (As a column vector [Bias;weight1;weight2;etc]) + X variable (Also column vector). Output is column vector
+% any no of variables. 2 weights (As a column vector [Bias;weight1;weight2;etc]) + X variables (Also column vector). Output is column vector
 
 % Calculate results given weights+data.
-function results = SLR(weights,variables);
+function results = LR(weights,variables);
 	variables = [ones(length(variables),1),variables];
 	if size(variables)(2) != size(weights)(1)
 		error("wrong weights to variables size")
 	end
-	results  = variable*weights; 
-end
-% Draw line as plot, given weights data, use above to get results needed.
-function PSLR(weights,variables);
-	results = SLR(weights,variables);
-	plot(variables,results);
+	results  = variables*weights; 
 end
 % Cost function, cost given results & targets.
 % Mean Squared Error cost function.
 % Input data in column vectors
-function tc = MSECSL(results,targets);
+function tc = MSECLR(results,targets);
 	if length(targets)!=length(results);
 		error("Input size mismatch");
 	end
@@ -27,9 +22,9 @@ end
 % Gradient descent for the above.
 % One batch weight change
 % Actually more just returns derivative of above MSECSL
-function DeltaWeights = DWSLGD(weights,variables,targets);
+function DeltaWeights = DWLRGD(weights,variables,targets);
 	% checking this before we add the bias 'variable'
-	if length(targets)!=length(variables);
+	if size(targets)(1)!=size(variables)(1);
 		error("Input size mismatch");
 	end
 	% using this since we wont ever add bias 'variable' to targets
@@ -40,32 +35,34 @@ function DeltaWeights = DWSLGD(weights,variables,targets);
 	% Doing this here so I can unify how I handle bias and weights.
 	variables = [ones(length(variables),1),variables];
 
-	DeltaWeights = 1/m*sum(variables'*results-targets));
+	DeltaWeights = 1/m*sum(variables'*(results-targets));
 end
 
 % Multibatch
 
-function FinalWeights = SLGDMB(weights,variable,targets,lRate,nIters);
+function FinalWeights = LRGDMB(weights,variables,targets,lRate,nIters);
 	iters = 0;
 	%tempweights
 	tWeights = weights;
 	while iters < nIters;
-		tWeights -= lRate*DWSLGD(tWeights,variable,targets);
+		tWeights -= lRate*DWSLGD(tWeights,variables,targets);
 		iters+=1;
 	end
 	FinalWeights = tWeights;
 end
 
-%Examples
-%slope 1 intercept 0
-s1i0x=[1;2;3;4;5;6;7;8;9;10];
-s1i0y=[1;2;3;4;5;6;7;8;9;10];
-s1i0w=[0;1];
-%slope 1 intercept 1
-s1i1x=s1i0x;
-s1i1y=s1i0y.+1;
-s1i1w=[1;1];
-%slope 2 intercept 0
-s2i0x=s1i0x;
-s2i0y=s1i0y*2;
-s2i0w=[0;2];
+
+x1y1x=1:1:10;
+x1y1y=1:1:10;
+x1y1z=2:2:20;
+
+x2y1x=1:1:10;
+x2y1y=1:1:10;
+x2y1z=3:3:30;
+
+x2y0x=1:1:10;
+x2y0y=1:1:10;
+x2y0z=2:2:20;
+
+%convient shortcut
+%  [x2y0x',x2y0y']
