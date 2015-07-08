3385c8d772a08d81589ce146d7a0516c0c5a3b27
DM <kingcocomango@gmail.com>
2015-07-08 11:26:53 +0200

    Thats that, now for examples and testing!

--- logo.m
+++ logo.m
@@ -12,10 +12,32 @@ end
 % Cost function
 function Cost = slogCFunc(results,targets),
 	m=size(results,1);
-	Cost=(-1/m)*sum(targets.*log(results)+(1-targets).*log(1-results));
+	Cost=(1/m)*sum(-targets.*log(results)-(1-targets).*log(1-results));
 end;
 % Gradient Descent 1-Batch Step
+function DeltaWeights = LogDW(weights,variables,targets);
+	% checking this before we add the bias 'variable'
+	if size(targets)(1)!=size(variables)(1);
+		error("Input size mismatch");
+	end
+	% using this since we wont ever add bias 'variable' to targets
+	m = length(targets);
 
+	% calculating results all in 1 go
+	results = LogR(weights,variables);
+	% Doing this here so I can unify how I handle bias and weights.
+	variables = [ones(length(variables),1),variables];
+	DeltaWeights = (variables'*(results-targets));
+end
 % Gradient Descent
-
+function FinalWeights = LogGD(weights,variables,targets,lRate,nIters);
+	iters = 0;
+	%tempweights
+	tWeights = weights;
+	while iters < nIters;
+		tWeights -= lRate*LogDW(tWeights,variables,targets);
+		iters+=1;
+	end
+	FinalWeights = tWeights;
+end
 % Example Data
