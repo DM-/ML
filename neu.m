e81eba0072c68790445a6b2f8667c9e4324c300f
DM <kingcocomango@gmail.com>
2015-07-09 05:40:04 +0200

    Starting on neural nets, outline set

---
+++ neu.m
@@ -0,0 +1,21 @@
+% This will be for neural nets
+% Need the math lib as usual
+source funcs.m
+
+% Now what I want to implement
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%This section is for feedforward results finding
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% A single neuron, takes data (row for sets, columns for variables) and weights (column) and activation function
+
+% A layer of neurons, takes the same data as above, weights (each neurons weights take a column), derives size of net from weights and activation function
+
+% A standard 3 layer network, takes data as above, weights arranged as above + 3rd dimension for each layer, derives size of net from weights and activation function
+
+% An arbritary size network, data as above, weights as above, derives size as above, and activation function
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%BackProp
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
